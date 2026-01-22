1e0ecaa256523b75d931879e259fe64d93e4070e
David <dsabalete@gmail.com>
2026-01-22 22:44:09 +0100

    add Notifications annd styles for notes app

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/notes.js";
 import Note from "./components/Note.jsx";
+import Notification from "./components/Notification.jsx";
+import Footer from "./components/Footer";
 
 const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -23,7 +26,12 @@ const App = (props) => {
         setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from server`);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`,
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
   };
@@ -50,6 +58,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
@@ -68,6 +77,7 @@ const App = (props) => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   );
 };
--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -3,7 +3,13 @@ const baseUrl = 'http://localhost:3001/notes'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
-    return request.then(response => response.data)
+    const nonExisting = {
+        id: 9999,
+        content: 'This note is not saved to server',
+        important: true
+    }
+    return request.then(response => response.data.concat(nonExisting))
+    //    return request.then(response => response.data)
 }
 
 const create = newObject => {
