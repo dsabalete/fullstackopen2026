ce853dbb00e0782a8eb5ded1930ac816aae9bc22
David <dsabalete@gmail.com>
2026-01-23 23:20:34 +0100

    Remove CORS dependency and update package configuration; add proxy settings for API in Vite config

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require('cors')
 const app = express()
 
 const generateId = () => {
@@ -9,7 +8,6 @@ const generateId = () => {
     return String(maxId + 1)
 }
 
-app.use(cors())
 app.use(express.json())
 app.use(express.static('dist'))
 
