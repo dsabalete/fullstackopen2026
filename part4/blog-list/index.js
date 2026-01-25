c654d9a1bf4599287323c62dbaf4c511d867d7e0
David <dsabalete@gmail.com>
2026-01-25 16:20:05 +0100

    Exercise 4.1

---
+++ part4/blog-list/index.js
@@ -0,0 +1,45 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const MONGODB_URI = process.env.MONGODB_URI
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+})
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+mongoose.connect(MONGODB_URI, { family: 4 })
+
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+    Blog.find({}).then((blogs) => {
+        response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+
+    blog.save().then((result) => {
+        response.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
