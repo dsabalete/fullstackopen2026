e4ea0add6e4d8bd090a2ef780e00ef6f795b4b53
David <dsabalete@gmail.com>
2026-01-26 12:34:18 +0100

    Add test suite for Note API and update environment configurations
    - Implement tests for retrieving notes and ensure JSON response format.
    - Modify MongoDB URI handling in config based on NODE_ENV.
    - Update logger to suppress console output during tests.
    - Add cross-env for setting environment variables in scripts.
    - Include supertest for API testing and update package dependencies.

--- part4/notes-backend/mongo.js
+++ part4/notes-backend/mongo.js
@@ -7,7 +7,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0.crmstwc.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+const url = `mongodb+srv://fullstack:${password}@cluster0.crmstwc.mongodb.net/testNoteApp?retryWrites=true&w=majority&appName=Cluster0`
 mongoose.set('strictQuery', false)
 
 mongoose.connect(url, { family: 4 })
@@ -20,7 +20,7 @@ const noteSchema = new mongoose.Schema({
 const Note = mongoose.model('Note', noteSchema)
 
 const note = new Note({
-  content: 'HTML is easy 2',
+  content: 'HTML is easy 4',
   important: true,
 })
 
