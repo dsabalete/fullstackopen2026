755f99c0e2465404890d77d8d8ee1cc188893ab8
David <dsabalete@gmail.com>
2026-01-29 18:34:57 +0100

    Exercise 4.15

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -4,6 +4,7 @@ const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
 const middleware = require('./utils/middleware.js')
 const blogsRouter = require('./controllers/blogs.js')
+const usersRouter = require('./controllers/users.js')
 
 const app = express()
 
@@ -22,6 +23,7 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use('/api/users', usersRouter)
 app.use('/api/blogs', blogsRouter)
 
 app.use(middleware.unknownEndpoint)
