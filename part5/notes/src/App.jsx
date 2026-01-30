87e66412c074d945dd8feec70f5377a46d4c69a9
David <dsabalete@gmail.com>
2026-01-30 18:17:16 +0100

    feat: add login in frontend

--- part5/notes/src/App.jsx
+++ part5/notes/src/App.jsx
@@ -1,19 +1,34 @@
 import { useState, useEffect } from "react";
 import noteService from "./services/notes.js";
+import loginService from "./services/login";
 import Note from "./components/Note.jsx";
 import Notification from "./components/Notification.jsx";
 import Footer from "./components/Footer";
+import LoginForm from "./components/LoginForm.jsx";
+import NoteForm from "./components/NoteForm.jsx";
 
 const App = (props) => {
   const [notes, setNotes] = useState(null);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
       setNotes(initialNotes);
     });
+  }, []);
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedNoteAppUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      noteService.setToken(user.token);
+    }
   }, []);
 
   if (!notes) {
@@ -59,11 +74,58 @@ const App = (props) => {
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const user = await loginService.login({ username, password });
+
+      window.localStorage.setItem("loggedNoteAppUser", JSON.stringify(user));
+      noteService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch {
+      setErrorMessage("wrong credentials");
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    }
+  };
+
+  const handleLogout = () => {
+    loginService.logout();
+    setUser(null);
+  };
+
   return (
     <div>
-      <h1>Notes</h1>
+      <h1>Notes app</h1>
       <Notification message={errorMessage} />
-      <div>
+
+      {!user && (
+        <LoginForm
+          username={username}
+          setUsername={setUsername}
+          password={password}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      )}
+      {user && (
+        <div>
+          <p>
+            {user.name} logged in <button onClick={handleLogout}>logout</button>
+          </p>
+          <NoteForm
+            newNote={newNote}
+            handleNoteChange={handleNoteChange}
+            addNote={addNote}
+          />
+        </div>
+      )}
+
+      <div style={{ marginTop: "2rem" }}>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
         </button>
@@ -77,10 +139,11 @@ const App = (props) => {
           />
         ))}
       </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      <NoteForm
+        newNote={newNote}
+        handleNoteChange={handleNoteChange}
+        addNote={addNote}
+      />
       <Footer />
     </div>
   );
---
+++ part5/notes/src/components/NoteForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NoteForm = ({ newNote, handleNoteChange, addNote }) => (
+  <form onSubmit={addNote}>
+    <input value={newNote} onChange={handleNoteChange} />
+    <button type="submit">save</button>
+  </form>
+);
+
+export default NoteForm;
