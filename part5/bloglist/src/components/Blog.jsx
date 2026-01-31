83c9f6b7b6d3472dbbea11ae74a4f86e2339d2bb
David <dsabalete@gmail.com>
2026-01-31 17:22:57 +0100

    Exercise 5.7

--- part5/bloglist/src/components/Blog.jsx
+++ part5/bloglist/src/components/Blog.jsx
@@ -1,7 +1,37 @@
-const Blog = ({ blog }) => (
-  <div>
-    <em>{blog.title}</em> - by {blog.author}
-  </div>
-);
+import { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    padding: "1rem",
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: "1rem",
+  };
+
+  return (
+    <div style={blogStyle}>
+      <p>
+        <em>{blog.title}</em> - by {blog.author}{" "}
+        <button onClick={() => setShowDetails(!showDetails)}>
+          {showDetails ? "hide" : "view"}
+        </button>
+      </p>
+      <div style={{ display: showDetails ? "block" : "none" }}>
+        <p>
+          url:{" "}
+          <a href={blog.url} target="_blank">
+            {blog.url}
+          </a>
+        </p>
+        <p>
+          likes: {blog.likes} <button>like</button>
+        </p>
+        <p>{blog.user && <span>added by {blog.user.name}</span>}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Blog;
