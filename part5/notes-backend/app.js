f9751e2fcab77743724ddba8caf98b4ceca117ad
David <dsabalete@gmail.com>
2026-02-03 16:01:03 +0100

    chore: end to end testing with playwright

--- part5/notes-backend/app.js
+++ part5/notes-backend/app.js
@@ -12,13 +12,13 @@ const app = express()
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-    .connect(config.MONGODB_URI, { family: 4 })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -27,6 +27,11 @@ app.use(middleware.requestLogger)
 app.use('/api/login', loginRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/notes', notesRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- part5/notes-backend/controllers/users.js
+++ part5/notes-backend/controllers/users.js
@@ -3,30 +3,33 @@ const usersRouter = require('express').Router()
 const User = require('../models/user.js')
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({}).populate('notes', { content: 1, important: 1 })
-    response.json(users)
+  const users = await User.find({}).populate('notes', {
+    content: 1,
+    important: 1,
+  })
+  response.json(users)
 })
 
 usersRouter.post('/', async (request, response) => {
-    const { username, name, password } = request.body
+  const { username, name, password } = request.body
 
-    const saltRounds = 10
-    const passwordHash = await bcrypt.hash(password, saltRounds)
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
 
-    if (!password || password.length < 8) {
-        console.log(`Password: '${password}'`)
-        return response.status(400).json({ error: 'Password too short!' })
-    }
+  if (!password || password.length < 8) {
+    console.log(`Password: '${password}'`)
+    return response.status(400).json({ error: 'Password too short!' })
+  }
 
-    const user = new User({
-        username,
-        name,
-        passwordHash,
-    })
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
 
-    const savedUser = await user.save()
+  const savedUser = await user.save()
 
-    response.status(201).json(savedUser)
+  response.status(201).json(savedUser)
 })
 
 module.exports = usersRouter
