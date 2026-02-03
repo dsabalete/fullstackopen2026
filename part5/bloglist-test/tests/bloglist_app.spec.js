aac5e2bb92cbd5d3b6d4790a360b0d850dd5d297
David <dsabalete@gmail.com>
2026-02-03 17:39:41 +0100

    Exercise 5.18

--- part5/bloglist-test/tests/bloglist_app.spec.js
+++ part5/bloglist-test/tests/bloglist_app.spec.js
@@ -1,8 +1,17 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
+const { loginWith } = require('./helper.js')
 
 describe('Blog app', () => {
-  beforeEach(async ({ page }) => {
-    await page.goto('http://localhost:5173')
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'Test User',
+        username: 'usertest',
+        password: 'secret',
+      },
+    })
+    await page.goto('/')
   })
 
   test('Login form is shown', async ({ page }) => {
@@ -11,4 +20,17 @@ describe('Blog app', () => {
     await expect(page.getByText('password')).toBeVisible()
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible()
   })
+
+  describe('Login', () => {
+    test('succeeds with correct credentials', async ({ page }) => {
+      loginWith(page, 'usertest', 'secret')
+      await expect(page.getByText('Test User logged in')).toBeVisible()
+    })
+
+    test('fails with wrong credentials', async ({ page }) => {
+      loginWith(page, 'usertest', 'wrong')
+      await expect(page.getByText('invalid username or password')).toBeVisible()
+    })
+  })
+
 })
