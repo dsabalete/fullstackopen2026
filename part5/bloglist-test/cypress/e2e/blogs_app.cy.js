104968c74451fc2c51e18ab83f33eb89ce893d25
David <dsabalete@gmail.com>
2026-02-04 18:32:51 +0100

    Exercise 5.18

--- part5/bloglist-test/cypress/e2e/blogs_app.cy.js
+++ part5/bloglist-test/cypress/e2e/blogs_app.cy.js
@@ -1,19 +1,42 @@
-describe("Blogs app", () => {
+describe('Blogs app', () => {
   beforeEach(function () {
-    cy.request("POST", "/api/testing/reset");
+    cy.request('POST', '/api/testing/reset')
     const user = {
-      name: "Test User",
-      username: "testuser",
-      password: "testpassword",
-    };
-    cy.request("POST", "/api/users/", user);
-    cy.visit("");
-  });
+      name: 'Test User',
+      username: 'testuser',
+      password: 'testpassword',
+    }
+    cy.request('POST', '/api/users/', user)
+    cy.visit('')
+  })
 
-  it("Login form is shown", function () {
-    cy.contains("Login");
-    cy.contains("username");
-    cy.contains("password");
-    cy.contains("login");
-  });
-});
+  it('Login form is shown', function () {
+    cy.contains('login').click()
+    cy.contains('username').should('be.visible')
+    cy.contains('password').should('be.visible')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.contains('button', 'login').click()
+      cy.contains('label', 'username').type('testuser')
+      cy.contains('label', 'password').type('testpassword')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with invalid username or password', function () {
+      cy.contains('button', 'login').click()
+      cy.contains('label', 'username').type('testuser')
+      cy.contains('label', 'password').type('wrongpassword')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in').should('not.exist')
+
+      cy.get('.error')
+        .should('contain', 'invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
+  })
+})
