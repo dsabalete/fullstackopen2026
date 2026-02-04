24a45d0fb03fd269a330e91f5d0fc6fe041c0f94
David <dsabalete@gmail.com>
2026-02-04 11:47:14 +0100

    chore: e2e with cypress

--- part5/notes-backend/app.js
+++ part5/notes-backend/app.js
@@ -1,34 +1,39 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const config = require('./utils/config.js')
-const logger = require('./utils/logger.js')
-const middleware = require('./utils/middleware.js')
-const notesRouter = require('./controllers/notes.js')
-const usersRouter = require('./controllers/users.js')
-const loginRouter = require('./controllers/login.js')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config.js");
+const logger = require("./utils/logger.js");
+const middleware = require("./utils/middleware.js");
+const notesRouter = require("./controllers/notes.js");
+const usersRouter = require("./controllers/users.js");
+const loginRouter = require("./controllers/login.js");
 
-const app = express()
+const app = express();
 
-logger.info('connecting to', config.MONGODB_URI)
+logger.info("connecting to", config.MONGODB_URI);
 
 mongoose
-    .connect(config.MONGODB_URI, { family: 4 })
-    .then(() => {
-        logger.info('connected to MongoDB')
-    })
-    .catch((error) => {
-        logger.error('error connection to MongoDB:', error.message)
-    })
+  .connect(config.MONGODB_URI, { family: 4 })
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
 
-app.use(express.static('dist'))
-app.use(express.json())
-app.use(middleware.requestLogger)
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use('/api/login', loginRouter)
-app.use('/api/users', usersRouter)
-app.use('/api/notes', notesRouter)
+app.use("/api/login", loginRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/notes", notesRouter);
 
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
+if (process.env.NODE_ENV === "test") {
+  const testingRouter = require("./controllers/testing.js");
+  app.use("/api/testing", testingRouter);
+}
 
-module.exports = app
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
