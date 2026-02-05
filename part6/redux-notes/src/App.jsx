388d5bde2c93015748539f0a4faf81b1848337ea
David <dsabalete@gmail.com>
2026-02-05 16:19:53 +0100

    part6-2

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,10 +1,12 @@
-import NoteForm from './components/NoteForm'
-import Notes from './components/Notes'
+import NoteForm from './components/NoteForm.jsx'
+import Notes from './components/Notes.jsx'
+import VisibilityFilter from './components/VisibilityFilter.jsx'
 
 const App = () => {
   return (
     <div>
       <NoteForm />
+      <VisibilityFilter />
       <Notes />
     </div>
   )
