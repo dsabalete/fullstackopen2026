4f454d5462dccf93f2f14fa3c39a8de9709cd2e8
David <dsabalete@gmail.com>
2026-02-05 19:02:27 +0100

    part6-4

--- part6/redux-notes/src/App.jsx
+++ part6/redux-notes/src/App.jsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+
 import NoteForm from './components/NoteForm.jsx'
 import Notes from './components/Notes.jsx'
 import VisibilityFilter from './components/VisibilityFilter.jsx'
+import { setNotes } from './reducers/noteReducer'
+import noteService from './services/notes'
 
 const App = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    noteService.getAll().then((notes) => dispatch(setNotes(notes)))
+  }, [dispatch])
+
   return (
     <div>
       <NoteForm />
--- part6/redux-notes/src/services/notes.js
+++ part6/redux-notes/src/services/notes.js
@@ -1,32 +1,28 @@
-import axios from 'axios'
-const baseUrl = '/api/notes'
+const baseUrl = 'http://localhost:3001/notes'
 
-let token = null
+const getAll = async () => {
+  const response = await fetch(baseUrl)
 
-const setToken = newToken => {
-  token = `Bearer ${newToken}`
-}
-
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
-
-const create = async newObject => {
-  const config = {
-    headers: { Authorization: token }
+  if (!response.ok) {
+    throw new Error('Failed to fetch notes')
   }
-  try {
-    const response = await axios.post(baseUrl, newObject, config)
-    return response.data
-  } catch (error) {
-    console.error('ERROR', error.response.data, { token })
+
+  const data = await response.json()
+  return data
+}
+
+const createNew = async (content) => {
+  const response = await fetch(baseUrl, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ content, important: false }),
+  })
+
+  if (!response.ok) {
+    throw new Error('Failed to create note')
   }
+
+  return await response.json()
 }
 
-const update = (id, newObject) => {
-  const request = axios.put(`${baseUrl}/${id}`, newObject)
-  return request.then(response => response.data)
-}
-
-export default { getAll, create, update, setToken }
+export default { getAll, createNew }
