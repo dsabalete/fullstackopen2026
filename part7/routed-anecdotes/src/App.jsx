e0e0cdb28f0a302001c40919b6ac7c213c76a7a8
David <dsabalete@gmail.com>
2026-02-07 00:46:32 +0100

    Exercise 7.5

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -7,60 +7,7 @@ import About from './components/About'
 import Footer from './components/Footer'
 import Anecdote from './components/Anecdote'
 import Notification from './components/Notification'
-
-import { useField } from './hooks'
-
-const CreateNew = ({ addNew }) => {
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    addNew({
-      content,
-      author,
-      info,
-      votes: 0,
-    })
-  }
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input
-            name="content"
-            type={content.type}
-            value={content.value}
-            onChange={content.onChange}
-          />
-        </div>
-        <div>
-          author
-          <input
-            name="author"
-            type={author.type}
-            value={author.value}
-            onChange={author.onChange}
-          />
-        </div>
-        <div>
-          url for more info
-          <input
-            name="info"
-            type={info.type}
-            value={info.value}
-            onChange={info.onChange}
-          />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  )
-}
+import CreateNew from './components/CreateNew'
 
 const App = () => {
   const navigate = useNavigate()
---
+++ part7/routed-anecdotes/src/components/CreateNew.jsx
@@ -0,0 +1,65 @@
+import { useField } from '../hooks'
+
+const CreateNew = ({ addNew }) => {
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    addNew({
+      content,
+      author,
+      info,
+      votes: 0,
+    })
+  }
+
+  const handleReset = (e) => {
+    e.preventDefault()
+    content.reset()
+    author.reset()
+    info.reset()
+  }
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input
+            name="content"
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
+          />
+        </div>
+        <div>
+          author
+          <input
+            name="author"
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
+          />
+        </div>
+        <div>
+          url for more info
+          <input
+            name="info"
+            type={info.type}
+            value={info.value}
+            onChange={info.onChange}
+          />
+        </div>
+        <button>create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CreateNew
