2ea901bed3173d1a24fd6eb186cee38f7c745003
David <dsabalete@gmail.com>
2026-02-06 13:38:33 +0100

    Exercise 6.21

--- part6/query-anecdotes/src/App.jsx
+++ part6/query-anecdotes/src/App.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
+import { useQuery } from '@tanstack/react-query'
 import { getAnecdotes } from './requests'
 
 import AnecdoteForm from './components/AnecdoteForm'
