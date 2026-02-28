2a23d97f9300783daaa3971bfbc888481284e368
David <dsabalete@gmail.com>
2026-02-28 09:44:33 +0100

    part8: fixing validations

--- part8/graphql-phonebook-frontend/src/components/PersonForm.jsx
+++ part8/graphql-phonebook-frontend/src/components/PersonForm.jsx
@@ -16,7 +16,14 @@ const PersonForm = ({ setError }) => {
   const submit = (event) => {
     event.preventDefault()
 
-    createPerson({ variables: { name, phone, street, city } })
+    createPerson({
+      variables: {
+        name,
+        street,
+        city,
+        phone: phone.length > 0 ? phone : undefined,
+      },
+    })
 
     setName('')
     setPhone('')
