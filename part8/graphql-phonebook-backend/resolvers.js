cc67915f9f6ff9a90ee00318a05083a07a9460bd
David <dsabalete@gmail.com>
2026-03-01 19:27:13 +0100

    part8: n+1 problem

--- part8/graphql-phonebook-backend/resolvers.js
+++ part8/graphql-phonebook-backend/resolvers.js
@@ -11,11 +11,14 @@ const resolvers = {
   Query: {
     personCount: async () => Person.collection.countDocuments(),
     allPersons: async (root, args) => {
+      console.log('Person.find')
       if (!args.phone) {
-        return Person.find({})
+        return Person.find({}).populate('friendOf')
       }
 
-      return Person.find({ phone: { $exists: args.phone === 'YES' } })
+      return Person.find({ phone: { $exists: args.phone === 'YES' } }).populate(
+        'friendOf',
+      )
     },
     findPerson: async (root, args) => Person.findOne({ name: args.name }),
     me: (root, args, context) => {
@@ -28,6 +31,16 @@ const resolvers = {
         street,
         city,
       }
+    },
+    friendOf: async (root) => {
+      console.log('User.find')
+      const friends = await User.find({
+        friends: {
+          $in: [root._id],
+        },
+      })
+
+      return friends
     },
   },
   Mutation: {
