09b1033e734185c091998194e339d51ff91f89bd
David <dsabalete@gmail.com>
2026-03-04 16:31:21 +0100

    part9: type guards

--- part9/flights/src/utils.ts
+++ part9/flights/src/utils.ts
@@ -1,13 +1,47 @@
-import { NewDiaryEntry } from './types';
+import { NewDiaryEntry, Weather } from './types';
+
+const isString = (text: unknown): text is string => {
+    return typeof text === 'string' || text instanceof String;
+};
+
+const parseComment = (comment: unknown): string => {
+    if (!comment || !isString(comment)) {
+        throw new Error('Incorrect or missing comment');
+    }
+
+    return comment;
+};
+
+const isDate = (date: string): boolean => {
+    return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+    if (!date || !isString(date) || !isDate(date)) {
+        throw new Error('Incorrect or missing date: ' + date);
+    }
+    return date;
+};
+
+const isWeather = (str: string): str is Weather => {
+    return ['sunny', 'rainy', 'cloudy', 'stormy'].includes(str);
+};
+
+const parseWeather = (weather: unknown): Weather => {
+    if (!weather || !isString(weather) || !isWeather(weather)) {
+        throw new Error('Incorrect or missing weather: ' + weather);
+    }
+    return weather;
+};
 
 export const toNewDiaryEntry = (object: unknown): NewDiaryEntry => {
     console.log(object); // now object is no longer unused
 
     const newEntry: NewDiaryEntry = {
-        weather: 'cloudy', // fake the return value
+        weather: parseWeather(object),
         visibility: 'great',
-        date: '2022-1-1',
-        comment: 'fake news'
+        date: parseDate(object),
+        comment: parseComment(object)
     };
 
     return newEntry;
