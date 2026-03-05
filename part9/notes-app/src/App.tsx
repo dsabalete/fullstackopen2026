8760d3ff6736bfa5302da3e642068f6d9abe15e7
David <dsabalete@gmail.com>
2026-03-05 19:45:40 +0100

    part9: react app with state

---
+++ part9/notes-app/src/App.tsx
@@ -0,0 +1,40 @@
+interface Note {
+  id: string;
+  content: string;
+}
+
+import { useState } from "react";
+
+const App = () => {
+  const [notes, setNotes] = useState<Note[]>([{ id: "1", content: "testing" }]);
+  const [newNote, setNewNote] = useState("");
+
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1),
+    };
+    setNotes(notes.concat(noteToAdd));
+    setNewNote("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          value={newNote}
+          onChange={(event) => setNewNote(event.target.value)}
+        />
+        <button type="submit">add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
