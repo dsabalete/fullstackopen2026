6cb9673cd2363b4b237a48d4d3205e143cc0809d
David <dsabalete@gmail.com>
2026-03-15 11:30:18 +0100

    part10: HTTP requests

--- part10/rate-repository-app/eslint.config.mjs
+++ part10/rate-repository-app/eslint.config.mjs
@@ -27,6 +27,7 @@ export default defineConfig([
     languageOptions: {
       globals: {
         ...reactNative.environments["react-native"]["react-native"],
+        fetch: "readonly",
       },
 
       parser: babelParser,
