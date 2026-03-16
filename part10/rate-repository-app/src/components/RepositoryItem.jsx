a962783983e342b1f2203c62a35fcfb547636d25
David <dsabalete@gmail.com>
2026-03-16 16:53:30 +0100

    Exercise 10.17

--- part10/rate-repository-app/src/components/RepositoryItem.jsx
+++ part10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View style={styles.container}>
+    <View testID="repositoryItem" style={styles.container}>
       <RepositoryHeader item={item} />
       <RepositoryStats item={item} />
     </View>
